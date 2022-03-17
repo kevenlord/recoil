99161e8cf6c6ea41537027129e30207130ee40fc
Keven Reis <39449222+kevenlord@users.noreply.github.com>
2022-03-17 17:05:56 -0300

    Correção
    O selector do de followers agora retorna os dados corretamente

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -8,17 +8,16 @@ export default function UserList() {
     const usernames = useRecoilValue(usernamesList) 
     const followerMap = useRecoilValue(userToFollowerMap)
 
-    console.log(JSON.stringify(followerMap))
-
     function getFollowers(username){
         return followerMap[username]
     }
-
+    console.log(usernames)
+    console.log(followerMap)
     return (
         <ul>
             {usernames.map(username =>(
                 <li key={username}>
-                    {username} 
+                    Usuário: {username}, possui {getFollowers(username)} seguidores!
                 </li>
             ))}
         </ul>
